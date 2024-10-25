23d3bb4f3e391e17f22159169bdd5d6e1d0cfe98
JuanDaGue <dj.jd2009@hotmail.com>
2024-10-24 19:23:16 -0500

    feature:db migration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,12 @@
-export default () => ({});
+module.exports = {
+    'strapi-neon-tech-db-branches': {
+      enabled: true,
+      config: {
+        neonApiKey: "mf9qk5din207k3u8rpwjvqg4w2tz8zimhpkshysst8rlx6s0nz7r6kcoro8v8090", // get it from here: https://console.neon.tech/app/settings/api-keys
+        neonProjectName: "ecomerse", // the neon project under wich your DB runs
+        neonRole: "db-migration_owner", // create it manually under roles for your project first
+        gitBranch: "main",
+        //(gitBranch: "main") // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+      }
+    },
+  }
